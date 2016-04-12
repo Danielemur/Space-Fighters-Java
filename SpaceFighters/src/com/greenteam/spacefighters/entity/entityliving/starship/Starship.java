5e94bd8385e6fbca22bf267c15113e1d43523842
Daniel Kelly <dk97@comcast.net>
2016-04-12 13:18:52 -0400

    Work in Player and Projectile.

--- SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/starship/Starship.java
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/starship/Starship.java
@@ -4,9 +4,29 @@ import com.greenteam.spacefighters.entity.entityliving.EntityLiving;
 import com.greenteam.spacefighters.stage.Stage;
 
 public abstract class Starship extends EntityLiving {
+	private int armorMultiplier;
+	private int weaponryMultiplier;
 
-	public Starship(Stage s, int health) {
+	public Starship(Stage s, int health, int armorMultiplier, int weaponryMultiply) {
 		super(s, health);
-	}
+	}	
+	
+	public abstract void fire();
 
+	public int getArmorMultiplier() {
+		return armorMultiplier;
+	}
+	
+	public void setArmorMultiplier(int mult) {
+		armorMultiplier = mult;
+	}
+	
+	public int getWeaponryMultiplier() {
+		return weaponryMultiplier;
+	}
+	
+	public void setWeaponryMultiplier(int mult) {
+		weaponryMultiplier = mult;
+	}
+	
 }
--- SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/starship/player/Player.java
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/starship/player/Player.java
@@ -2,13 +2,20 @@ package com.greenteam.spacefighters.entity.entityliving.starship.player;
 
 import java.awt.Graphics;
 
+import com.greenteam.spacefighters.entity.entityliving.projectile.Projectile;
 import com.greenteam.spacefighters.entity.entityliving.starship.Starship;
 import com.greenteam.spacefighters.stage.Stage;
 
 public class Player extends Starship {
+	private static final int DEFAULTARMORLEVEL = 0;
+	private static final int DEFAULTWEAPONRYLEVEL= 0;
+	private static final int DEFAULTWEAPONRYHEALTH = 1;
+	private static final int FIREDRAIN = 20;
+	private static final int FULLCHARGE = 100;
+	private static int chargeLevel;
 	
 	public Player(Stage s, int health) {
-		super(s, health);
+		super(s, health, DEFAULTARMORLEVEL, DEFAULTWEAPONRYLEVEL);
 	}
 
 	@Override
@@ -25,6 +32,15 @@ public class Player extends Starship {
 	@Override
 	public Class<?> getSource() {
 		return this.getClass();
+	}
+	
+	@Override
+	public void fire() {
+	    if (chargeLevel > FIREDRAIN) {
+	        int damage = 10 * (getWeaponryMultiplier() + 1);
+	        stage.add(new Projectile(stage, DEFAULTWEAPONRYHEALTH, damage, getSource()));
+	        chargeLevel -= FIREDRAIN;
+	    }
 	}
 
 }
