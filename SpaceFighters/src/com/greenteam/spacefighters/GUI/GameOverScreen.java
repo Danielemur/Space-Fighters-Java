860765d10322053985a9f29e821c9ff319286703
Jason Li <jcl01010011@gmail.com>
2016-04-17 18:12:45 -0400

    Title screen, Game Over screen prelim

---
+++ SpaceFighters/src/com/greenteam/spacefighters/GUI/GameOverScreen.java
@@ -0,0 +1,12 @@
+package com.greenteam.spacefighters.GUI;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameOverScreen extends JPanel {
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+}
