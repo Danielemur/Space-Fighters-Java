032fc0a890b24253b3e75d988cfd4e92ef66e76d
Daniel Kelly <dk97@comcast.net>
2016-04-20 13:20:37 -0400

    ForceField WIP.

--- SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/HealthRestorePowerup.java
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/HealthRestorePowerup.java
@@ -1,31 +1,11 @@
 package com.greenteam.spacefighters.entity.entityliving.powerup;
 
-import com.greenteam.spacefighters.common.Vec2;
-import com.greenteam.spacefighters.entity.Entity;
-import com.greenteam.spacefighters.entity.entityliving.EntityLiving;
-import com.greenteam.spacefighters.entity.entityliving.starship.player.Player;
 import com.greenteam.spacefighters.stage.Stage;
 
 public class HealthRestorePowerup extends Powerup {
-	private static final int SELECT_NEW_POSITION_INTERVAL = 1000;
-	private static final double SPAWNDIST = 400.0D;
-	private static final int SPEED = 100;
-	private static final int TIME_ON_SCREEN = 15000;
-	private static final int BEGIN_FADING_TIME = 500;
-
-	private Vec2 randpos;
-	private int time;
 
 	public HealthRestorePowerup(Stage s) {
 		super(s);
-		time = SELECT_NEW_POSITION_INTERVAL;
-		timeRemaining = TIME_ON_SCREEN;
-		
-		this.setPosition(this.randSpawnPos(s.getPlayer(), SPAWNDIST));
-		this.setOrientation(new Vec2(0,-1));
-		
-		randpos = new Vec2(Stage.WIDTH * Math.random(), Stage.HEIGHT * Math.random());
-		
 		this.setTexture(Powerup.getTexFromEnum(PowerupColor.RED));
 		if (this.getTexture() != null) {
 			couldLoadImage = true;
@@ -35,41 +15,8 @@ public class HealthRestorePowerup extends Powerup {
 	}
 	
 	@Override
-	public void update(int ms) {
-		super.update(ms);
-		time -= ms;
-		if (time <= 0) {
-			time = SELECT_NEW_POSITION_INTERVAL;
-			randpos = new Vec2(Stage.WIDTH * Math.random(), Stage.HEIGHT * Math.random());
-		}
-		this.setVelocity(randpos.subtract(this.getPosition()).normalize().scale(SPEED));
-		if (this.getHealth() <= 0) {
-			stage.setScore(stage.getScore() + this.getPointValue());
-		}
-		for (Entity e : this.getStage().getEntities()) {
-			if (e == this) continue;
-			if ((e.getPosition().distance(this.getPosition()) < this.getRadius() + e.getRadius()) &&
-					(e instanceof Player)) {
-				this.setHealth(this.getHealth() - ((EntityLiving)e).getDamage());
-			}
-		}
-		timeRemaining -= ms;
-		if (timeRemaining <= 0) this.remove();
-	}
-	
-	@Override
-	public double getRadius() {
-		return 24;
-	}
-	
-	@Override
 	public int getDamage() {
 		return -20;
-	}
-	
-	@Override
-	public int getBeginFadingTime() {
-		return BEGIN_FADING_TIME;
 	}
 
 	@Override
--- SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/Powerup.java
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/Powerup.java
@@ -12,19 +12,35 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 import com.greenteam.spacefighters.common.Vec2;
+import com.greenteam.spacefighters.entity.Entity;
 import com.greenteam.spacefighters.entity.entityliving.EntityLiving;
+import com.greenteam.spacefighters.entity.entityliving.starship.player.Player;
 import com.greenteam.spacefighters.stage.Stage;
 
 public abstract class Powerup extends EntityLiving {
+	private static final int SELECT_NEW_POSITION_INTERVAL = 1000;
+	private static final double SPAWNDIST = 400.0D;
+	private static final int SPEED = 100;
+	private static final int TIME_ON_SCREEN = 15000;
+	private static final int BEGIN_FADING_TIME = 500;
+
 	protected boolean couldLoadImage;
-	protected int timeRemaining;
+	private int timeRemaining;
+	private Vec2 randpos;
+	private int time;
 	
 	public Powerup(Stage s) {
 		super(s, 1);
+		time = SELECT_NEW_POSITION_INTERVAL;
+		timeRemaining = TIME_ON_SCREEN;
+		
+		this.setPosition(this.randSpawnPos(s.getPlayer(), SPAWNDIST));
+		this.setOrientation(new Vec2(0,-1));
+		
+		randpos = new Vec2(Stage.WIDTH * Math.random(), Stage.HEIGHT * Math.random());
 	}
 	
 	public abstract java.awt.Color noTextureColor();
-	public abstract int getBeginFadingTime();
 	
 	@Override
 	public void render(Graphics g) {
@@ -35,8 +51,8 @@ public abstract class Powerup extends EntityLiving {
 			double imagemidy = this.getTexture().getHeight(null)/2;
 			AffineTransform tf = AffineTransform.getRotateInstance(angle, imagemidx, imagemidy);
 			AffineTransformOp op = new AffineTransformOp(tf, AffineTransformOp.TYPE_BILINEAR);
-			if ((timeRemaining <= getBeginFadingTime()) && (timeRemaining >= 0)) {
-				float opacity = timeRemaining/(float)getBeginFadingTime();
+			if ((timeRemaining <= BEGIN_FADING_TIME) && (timeRemaining >= 0)) {
+				float opacity = timeRemaining/(float)BEGIN_FADING_TIME;
 				opacity = (float)Math.pow(opacity, 0.25);
 				Composite oldComposite = ((Graphics2D)g).getComposite();
 				((Graphics2D)g).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
@@ -50,6 +66,34 @@ public abstract class Powerup extends EntityLiving {
 			g.setColor(Color.RED);
 			g.fillRect((int)pos.getX(), (int)pos.getY(), 10, 10);
 		}
+	}
+	
+	@Override
+	public void update(int ms) {
+		super.update(ms);
+		time -= ms;
+		if (time <= 0) {
+			time = SELECT_NEW_POSITION_INTERVAL;
+			randpos = new Vec2(Stage.WIDTH * Math.random(), Stage.HEIGHT * Math.random());
+		}
+		this.setVelocity(randpos.subtract(this.getPosition()).normalize().scale(SPEED));
+		if (this.getHealth() <= 0) {
+			stage.setScore(stage.getScore() + this.getPointValue());
+		}
+		for (Entity e : this.getStage().getEntities()) {
+			if (e == this) continue;
+			if ((e.getPosition().distance(this.getPosition()) < this.getRadius() + e.getRadius()) &&
+					(e instanceof Player)) {
+				this.setHealth(this.getHealth() - ((EntityLiving)e).getDamage());
+			}
+		}
+		timeRemaining -= ms;
+		if (timeRemaining <= 0) this.remove();
+	}
+	
+	@Override
+	public double getRadius() {
+		return 24;
 	}
 	
 	@Override
