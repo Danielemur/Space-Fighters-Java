b5338d1afacc91ebc618af150f09d0b9b8850cdd
Daniel Kelly <dk97@comcast.net>
2016-04-24 00:46:12 -0400

    Actually commiting the augmented health powerup.

---
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerupcontainer/HealthBoostPowerupContainer.java
@@ -0,0 +1,27 @@
+package com.greenteam.spacefighters.entity.entityliving.powerupcontainer;
+
+import com.greenteam.spacefighters.stage.Stage;
+
+public class HealthBoostPowerupContainer extends PowerupContainer {
+
+	public HealthBoostPowerupContainer(Stage s) {
+		super(s);
+		this.setTexture(PowerupContainer.getTexFromEnum(PowerupColor.GREEN));
+		if (this.getTexture() != null) {
+			couldLoadImage = true;
+		} else {
+			couldLoadImage = false;
+		}
+	}
+	
+	@Override
+	public int getDamage() {
+		return -25;
+	}
+
+	@Override
+	public java.awt.Color noTextureColor() {
+		return java.awt.Color.GREEN;
+	}
+
+}
