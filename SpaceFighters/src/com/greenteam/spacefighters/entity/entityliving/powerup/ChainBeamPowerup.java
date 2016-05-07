907c9b6bd358961c8e3bc6d597b70861881f4093
Daniel Kelly <dk97@comcast.net>
2016-05-06 20:54:38 -0400

    Fixed ChainBeam rendering.

--- SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/ChainBeamPowerup.java
+++ SpaceFighters/src/com/greenteam/spacefighters/entity/entityliving/powerup/ChainBeamPowerup.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -19,14 +21,14 @@ import com.greenteam.spacefighters.stage.Stage;
 
 public class ChainBeamPowerup extends Powerup {
 	private static final int DURATION = Integer.MAX_VALUE;
-	private static final double TRAVELDISTANCE= 1000;
+	private static final double TRAVELDISTANCE= 2000;
 	private static final int SCANINTERVAL = 25;
 	private static final double INNERRADIUS = 30;
 	private static final double OUTERRADIUS = 40;
-	private static final double INNERWIDTH = 40;
-	private static final double OUTERWIDTH = 60;
-	private final Color INNERCOLOR = new Color(178, 21, 220, 127);
-	private final Color OUTERCOLOR = new Color(178, 21, 220, 63);
+	private static final double INNERWIDTH = 20;
+	private static final double OUTERWIDTH = 40;
+	private final Color INNERCOLOR = new Color(138, 21, 200, 127);
+	private final Color OUTERCOLOR = new Color(138, 21, 200, 63);
 	private LinkedHashSet<Entity> targets;
 	private double remainingDistance;
 	private int elapsedTime;
@@ -44,19 +46,10 @@ public class ChainBeamPowerup extends Powerup {
 		Vec2 midPt = p2.midpoint(p1);
 		double angle = dir.multiply(new Vec2(1.0, -1.0)).angle();
 		double length = p2.distance(p1);
-
 		Rectangle2D rect = new Rectangle2D.Double(midPt.getX() - width / 2, midPt.getY() - length /2, width, length);
 		AffineTransform tr = new AffineTransform();
 		tr.rotate(angle, midPt.getX(), midPt.getY());
-		System.out.println("P1:\t" + p1 +
-						   "\nP2:\t" + p2 +
-						   "\nWidth:\t" + width +
-						   "\nDir:\t" + dir +
-						   "\nMidPt:\t" + midPt +
-						   "\nAngle:\t" + angle +
-						   "\nLength:\t" + length);
 		return tr.createTransformedShape(rect);
-		//return rect;
 	}
 
 	@Override
@@ -68,32 +61,33 @@ public class ChainBeamPowerup extends Powerup {
 			entities.addAll(targets);
 
 			Vec2 entPos = entities.get(0).getPosition();
-
+			
 			g2.setColor(OUTERCOLOR.toAWTColor());
-			g.fillOval((int)(entPos.getX() - OUTERRADIUS), (int)(entPos.getY() - OUTERRADIUS), (int)(2 * OUTERRADIUS), (int)(2 * OUTERRADIUS));
+			Area outerArea = new Area();
+			outerArea.add(new Area(new Ellipse2D.Double(entPos.getX() - OUTERRADIUS, entPos.getY() - OUTERRADIUS, 2 * OUTERRADIUS, 2 * OUTERRADIUS)));
 
 			for(int i = 0; i < entities.size() - 1; i++) {
 				Entity e1 = entities.get(i);
 				Entity e2 = entities.get(i + 1);
 				Vec2 pos1 = e1.getPosition();
 				Vec2 pos2 = e2.getPosition();
-				g.fillOval((int)(pos2.getX() - OUTERRADIUS), (int)(pos2.getY() - OUTERRADIUS), (int)(2 * OUTERRADIUS), (int)(2 * OUTERRADIUS));
-
-				g2.fill(rectBetween(pos1, pos2, OUTERWIDTH));
+				outerArea.add(new Area(new Ellipse2D.Double(pos2.getX() - OUTERRADIUS, pos2.getY() - OUTERRADIUS, 2 * OUTERRADIUS, 2 * OUTERRADIUS)));
+				outerArea.add(new Area(rectBetween(pos1, pos2, OUTERWIDTH)));
 			}
-
+			g2.fill(outerArea);
 			g2.setColor(INNERCOLOR.toAWTColor());
-			g.fillOval((int)(entPos.getX() - INNERRADIUS), (int)(entPos.getY() - INNERRADIUS), (int)(2 * INNERRADIUS), (int)(2 * INNERRADIUS));
+			Area innerArea = new Area();
+			innerArea.add(new Area(new Ellipse2D.Double(entPos.getX() - INNERRADIUS, entPos.getY() - INNERRADIUS, 2 * INNERRADIUS, 2 * INNERRADIUS)));
 
 			for(int i = 0; i < entities.size() - 1; i++) {
 				Entity e1 = entities.get(i);
 				Entity e2 = entities.get(i + 1);
 				Vec2 pos1 = e1.getPosition();
 				Vec2 pos2 = e2.getPosition();
-				g.fillOval((int)(pos2.getX() - INNERRADIUS), (int)(pos2.getY() - INNERRADIUS), (int)(2 * INNERRADIUS), (int)(2 * INNERRADIUS));
-
-				g2.fill(rectBetween(pos1, pos2, INNERWIDTH));
+				innerArea.add(new Area(new Ellipse2D.Double(pos2.getX() - INNERRADIUS, pos2.getY() - INNERRADIUS, 2 * INNERRADIUS, 2 * INNERRADIUS)));
+				innerArea.add(new Area(rectBetween(pos1, pos2, INNERWIDTH)));
 			}
+			g2.fill(innerArea);
 		}
 
 	}
@@ -141,7 +135,10 @@ public class ChainBeamPowerup extends Powerup {
 				
 				Enemy target = (Enemy)s.getNearestEntity(currentTarget, (Set<Entity>)targets, Enemy.class);
 				
-				double dist = this.getPosition().distance(target.getPosition()); 
+				double dist = Double.POSITIVE_INFINITY;
+				if (target != null) {
+					dist = this.getPosition().distance(target.getPosition()); 
+				}
 				if (dist <= remainingDistance) {
 					remainingDistance -= dist;
 					target.setUpdatable(false);
