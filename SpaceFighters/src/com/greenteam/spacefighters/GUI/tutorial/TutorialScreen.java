b09e01a74b8a6a9ab1849c35a22556ad78decbb9
Daniel Kelly <dk97@comcast.net>
2016-05-18 22:15:53 -0400

    Tutorial Screen formatting.

--- SpaceFighters/src/com/greenteam/spacefighters/GUI/tutorial/TutorialScreen.java
+++ SpaceFighters/src/com/greenteam/spacefighters/GUI/tutorial/TutorialScreen.java
@@ -24,6 +24,7 @@ public abstract class TutorialScreen extends JPanel implements ActionListener{
 	private static final int NUM_STARS = 120;
 	
 	protected Window window;
+	protected JLabel title;
 	protected JPanel centerGrid;
 	protected JButton prevScreen;
 	protected JButton nextScreen;
@@ -36,9 +37,23 @@ public abstract class TutorialScreen extends JPanel implements ActionListener{
 		this.setOpaque(true);
 		this.setBackground(Color.BLACK);
 		this.setLayout(new GridBagLayout());
+		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.weighty = 0;
+		gbc.weightx = 0;
+		gbc.gridwidth = 2;
+		gbc.anchor = GridBagConstraints.CENTER;
+		title = new JLabel("Tutorial");
+		title.setOpaque(false);
+		title.setForeground(Color.LIGHT_GRAY);
+		title.setFont(title.getFont().deriveFont(24.0f));
+		this.add(title, gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
 		gbc.weighty = 1;
 		gbc.weightx = 1;
 		gbc.anchor = GridBagConstraints.CENTER;
@@ -49,7 +64,7 @@ public abstract class TutorialScreen extends JPanel implements ActionListener{
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
-		gbc.gridy = 1;
+		gbc.gridy = 2;
 		gbc.weighty = 0;
 		JPanel buttonPanel = new JPanel();
 		prevScreen = new JButton("Back");
