058f4c13161606691b9dfcb150726670a24853c5
Daniel Kelly <dk97@comcast.net>
2016-05-19 13:01:33 -0400

    Difficulty Settings.

--- SpaceFighters/src/com/greenteam/spacefighters/GUI/SettingsScreen.java
+++ SpaceFighters/src/com/greenteam/spacefighters/GUI/SettingsScreen.java
@@ -14,9 +14,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import com.greenteam.spacefighters.GUI.Window;
 import com.greenteam.spacefighters.entity.entityliving.starship.player.Player;
 import com.greenteam.spacefighters.entity.entityliving.starship.player.Player.PlayerShipColor;
+import com.greenteam.spacefighters.stage.LevelLoader;
 
 public class SettingsScreen extends JPanel implements ActionListener{
 	private static final long serialVersionUID = 3798035225065059125L;
@@ -24,10 +26,12 @@ public class SettingsScreen extends JPanel implements ActionListener{
 	private static final int NUM_STARS = 120;
 	
 	protected Window window;
+	protected JLabel difficultyinfo;
 	protected JLabel inputInfo;
 	protected JLabel colorInfo;
 	protected JPanel centerGrid;
 	protected JButton inputSelect;
+	protected JButton difficultySelect;
 	protected ShipButton colorSelect;
 	protected JButton back;
 
@@ -47,6 +51,35 @@ public class SettingsScreen extends JPanel implements ActionListener{
 		gbc.weightx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
 		gbc.insets = new Insets(20, 20, 0, 0);
+		String difficultyText;
+		if (window.getStage().getLevelLoader().isInEasyMode()) {
+			difficultyText = "EASY";
+		} else {
+			difficultyText = "HARD";
+		}
+		difficultyinfo = new JLabel("Current Difficulty:     " + difficultyText);
+		difficultyinfo.setOpaque(false);
+		difficultyinfo.setForeground(Color.LIGHT_GRAY);
+		this.add(difficultyinfo, gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.weighty = 0;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(20, 0, 0, 20);
+		difficultySelect = new JButton("Easy mode");
+		difficultySelect.addActionListener(this);
+		this.add(difficultySelect, gbc);		
+		
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weighty = 1;
+		gbc.weightx = 1;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(20, 20, 0, 0);
 		inputInfo = new JLabel("Toggle user input mode:");
 		inputInfo.setOpaque(false);
 		inputInfo.setForeground(Color.LIGHT_GRAY);
@@ -54,7 +87,7 @@ public class SettingsScreen extends JPanel implements ActionListener{
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 1;
-		gbc.gridy = 0;
+		gbc.gridy = 1;
 		gbc.weighty = 0;
 		gbc.weightx = 0;
 		gbc.anchor = GridBagConstraints.WEST;
@@ -65,7 +98,7 @@ public class SettingsScreen extends JPanel implements ActionListener{
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
-		gbc.gridy = 1;
+		gbc.gridy = 2;
 		gbc.weighty = 1;
 		gbc.weightx = 1;
 		gbc.anchor = GridBagConstraints.WEST;
@@ -77,7 +110,7 @@ public class SettingsScreen extends JPanel implements ActionListener{
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 1;
-		gbc.gridy = 1;
+		gbc.gridy = 2;
 		gbc.weighty = 0;
 		gbc.weightx = 0;
 		gbc.anchor = GridBagConstraints.WEST;
@@ -90,7 +123,7 @@ public class SettingsScreen extends JPanel implements ActionListener{
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
-		gbc.gridy = 2;
+		gbc.gridy = 3;
 		gbc.gridwidth = 2;
 		gbc.insets = new Insets(20, 20, 20, 20);
 		back = new JButton("Back");
@@ -109,6 +142,18 @@ public class SettingsScreen extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent ev) {
 		if (ev.getSource() == back) {
 			((CardLayout)this.getParent().getLayout()).show(window.getContentPane(), Window.TITLE_SCREEN);
+		} else if (ev.getSource() == difficultySelect) {
+			LevelLoader loader = window.getStage().getLevelLoader();
+			loader.setEasyMode(!loader.isInEasyMode());
+			if (loader.isInEasyMode()) {
+				String difficultyText = "EASY";
+				difficultyinfo.setText("Current Difficulty:     " + difficultyText);
+				difficultySelect.setText("Hard mode");
+			} else {
+				String difficultyText = "HARD";
+				difficultyinfo.setText("Current Difficulty:     " + difficultyText);
+				difficultySelect.setText("Easy mode");
+			}
 		} else if (ev.getSource() == inputSelect) {
 			window.setMouseInput(!window.useMouseInput());
 			if (window.useMouseInput())
